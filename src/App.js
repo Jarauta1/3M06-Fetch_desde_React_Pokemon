718dcee1c23a74c48bb4f51a7e289ec76a799b88
Diego Jarauta <diegojarauta8@gmail.com>
2020-12-07 16:59:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  let url = "https://pokeapi.co/api/v2/type/"
+ 
+  let [data,setData] = useState([])
+ 
+  let [pokemon,setPokemon] = useState([])
+  let [num,setNum] = useState(0)
+  let [nombresAleatorios,setNombresAleatorios] = useState(0)
+  let [eleccion,setEleccion] = useState("")
+
+  useEffect(function(){
+   
+    fetch(url).then(respuesta=>respuesta.json()).then(datos=>{
+      setData(datos.results)
+    })
+  },[num])
+  
+  let mostrarTipo = data.map(tipo => {
+    return(
+<option value={tipo.url}>{tipo.name}</option>
+    )
+  })
+
+ function tipoElegido(e) {  
+   setEleccion(e.target.value)
+   setNombresAleatorios(nombresAleatorios+1)
+ }
+
+ useEffect(function(){
+  
+  fetch(eleccion).then(respuesta=>respuesta.json()).then(datos=>{
+    setPokemon(datos.pokemon)    
+  })
+},[nombresAleatorios])
+
+ if ( pokemon.length !=0){
+  return (<>
+    <select  onChange={tipoElegido} name="select">
+                <option value="--">--</option>
+                 {mostrarTipo}
+            </select> 
+            <div>
+              <ul>
+                <li>{pokemon[parseInt(Math.random()*(pokemon.length))].pokemon.name}</li>
+                <li>{pokemon[parseInt(Math.random()*(pokemon.length))].pokemon.name}</li>
+                <li>{pokemon[parseInt(Math.random()*(pokemon.length))].pokemon.name}</li>
+              </ul>
+            </div>
+  </>);
+ } else {
+  return (<>
+    <select  onChange={tipoElegido} name="select">
+                <option value="--">--</option>
+                 {mostrarTipo}
+            </select> 
+  </>);
+
+ }  
 }
 
 export default App;
